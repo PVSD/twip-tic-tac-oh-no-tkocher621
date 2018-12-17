a2314b69cf90ecfed523340c6ca42413c69beee4
tk304 <tk304@pvsd.org>
2018-12-17 11:21:09 -0500

    cool

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,43 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
+    public static void PvP()
+    {
+        TicTacToe t = new TicTacToe();
+        t.DrawBoard();
+        // check if anyone has won
+        System.out.println("Player 1 goes first!");
+        while (!t.CheckWin())
+        {
+
+            // prompt input above
+            t.ChangePlayer();
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Which mode would you like to play? \n" +
+        "1) Player vs. Player" +
+        "2) Player vs. Computer" +
+        "3) Computer vs. Computer");
+
+        String input = scan.nextLine();
+
+        switch (input)
+        {
+            case "1":
+            {
+                PvP();
+                break;
+            }
+        }
+
+
     }
 }
